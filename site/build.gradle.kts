522b738455e15f1fe8147cc5ceea75fedb985349
papel1 <papel1.uni@gmail.com>
2025-11-13 00:13:19 +0100

    Revert to Kobweb 0.12.3 with Font Awesome icons support

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -4,7 +4,6 @@ import com.varabyte.kobweb.gradle.application.util.configAsKobwebApplication
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.jetbrains.compose)
-    alias(libs.plugins.jetbrains.compose.compiler)
     alias(libs.plugins.kobweb.application)
 //    alias(libs.plugins.kobwebx.markdown)
 }
